9f902365fbe9e94bfcae9457db4a0effb948c669
Lukas <lukas_canji@hotmail.com>
2024-03-23 15:53:54 -0400

    Initial Commit

---
+++ projectPrototype/projectPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectPrototypeApp.swift
+//  projectPrototype
+//
+//  Created by Lukas Canji on 2024-03-23.
+//
+
+import SwiftUI
+
+@main
+struct projectPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
