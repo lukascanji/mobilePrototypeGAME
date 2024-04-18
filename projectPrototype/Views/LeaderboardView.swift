413fc1c84493a0c40fc737756edddc30193382a8
lukascanji <40840808+lukascanji@users.noreply.github.com>
2024-04-17 23:15:23 -0400

    first commit

---
+++ projectPrototype/Views/LeaderboardView.swift
@@ -0,0 +1,18 @@
+//
+//  LeaderboardView.swift
+//  projectPrototype
+//
+//  Created by Lukas Canji on 2024-04-17.
+//
+
+import SwiftUI
+
+struct LeaderboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LeaderboardView()
+}
---
+++ projectPrototype/Views/StartView.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  projectPrototype
+//
+//  Created by Lukas Canji on 2024-04-17.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartView()
+}
